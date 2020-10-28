a18ef4df14f42eeb9ac62b73e56624dff075d8d9
PhillipRojkov <rojkovphillip@gmail.com>
2020-10-27 17:28:12 -0700

    Implemented PID forward and backward auto functions

--- 1010GBase/src/main.cpp
+++ 1010GBase/src/main.cpp
@@ -58,9 +58,123 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+double initialSpeed = 10; //Speed from which a robot accelerates in autonomous functions
+
+void RedLeftCorner () {
+
+}
+
+void resetDriveEncoder() { //Resets all driver encoder positions to zero
+  DriveBL.resetPosition();
+  DriveBR.resetPosition();
+  DriveFL.resetPosition();
+  DriveFR.resetPosition();
+}
+
+double avgDriveEncoder() { //Returns average of all driver encoder positions
+  return (DriveBL.position(vex::rotationUnits::deg) + 
+        DriveBR.position(vex::rotationUnits::deg) +
+        DriveFL.position(vex::rotationUnits::deg) +
+        DriveBR.position(vex::rotationUnits::deg)) /4;
+}
+
+void drive(int dir, double speed) { //Drive forward (dir = 1) or backward (dir = -1)
+    DriveBL.spin(forward, speed * dir, vex::velocityUnits::pct);
+    DriveBR.spin(forward, speed * dir, vex::velocityUnits::pct);
+    DriveFL.spin(forward, speed * dir, vex::velocityUnits::pct);
+    DriveFR.spin(forward, speed * dir, vex::velocityUnits::pct);
+}
+
+void brakeDrive() { //Stop the drive using brake mode brake
+  DriveBL.stop(vex::brake);
+  DriveBR.stop(vex::brake);
+  DriveFL.stop(vex::brake);
+  DriveFR.stop(vex::brake);
+}
+
+void autoForward(double degrees, double iDeg, double fDeg, double speed) { //Forward auto function. degrees > iDeg + fDeg
+ resetDriveEncoder();
+ 
+ while (avgDriveEncoder() < iDeg) { //Accelerate for the initial degrees (iDeg)
+    double accelerate = speed * avgDriveEncoder() / iDeg;
+
+    if (accelerate < initialSpeed) { //Make sure that the motors never move slower than initalSpeed
+      accelerate = initialSpeed;
+    }
+
+    //Run the drive
+    drive(1, accelerate);
+
+    wait(10, msec);
+  }
+  while (avgDriveEncoder() < degrees - fDeg) { //Drive at speed up until you reach final degrees (fDeg) threshold
+    //Run the drive
+    drive(1, speed);
+
+    wait(10, msec);
+  }
+  while (avgDriveEncoder() < degrees) { //Decellerate for the final degrees (fDeg)
+    double deccelerate = speed - speed * avgDriveEncoder() / degrees;
+
+    if (deccelerate < initialSpeed) { //Make sure that the motors never move slower than initalSpeed
+      deccelerate = initialSpeed;
+    }
+
+    //Run the drive
+    drive(1, deccelerate);
+
+    wait(10, msec);
+  }
+
+  //Stop the drive
+  brakeDrive();
+}
+
+void autoBackward(double degrees, double iDeg, double fDeg, double speed) { //Backward auto function. degrees > iDeg + fDeg
+ resetDriveEncoder();
+ 
+ while (fabs(avgDriveEncoder()) < iDeg) { //Accelerate for the initial degrees (iDeg)
+    double accelerate = speed * avgDriveEncoder() / iDeg;
+
+    if (accelerate < initialSpeed) { //Make sure that the motors never move slower than initalSpeed
+      accelerate = initialSpeed;
+    }
+
+    //Run the drive
+    drive(-1, accelerate);
+
+    wait(10, msec);
+  }
+  while (fabs(avgDriveEncoder()) < degrees - fDeg) { //Drive at speed up until you reach final degrees (fDeg) threshold
+    //Run the drive
+    drive(-1, speed);
+
+    wait(10, msec);
+  }
+  while (fabs(avgDriveEncoder()) < degrees) { //Decellerate for the final degrees (fDeg)
+    double deccelerate = speed - speed * avgDriveEncoder() / degrees;
+
+    if (deccelerate < initialSpeed) { //Make sure that the motors never move slower than initalSpeed
+      deccelerate = initialSpeed;
+    }
+
+    //Run the drive
+    drive(-1, deccelerate);
+
+    wait(10, msec);
+  }
+  
+  //Stop the drive
+  brakeDrive();
+}
+
 void autonomous(void) {
   // ..........................................................................
   // Insert autonomous user code here.
+
+  autoForward(720, 180, 180, 100);
+  autoBackward(720, 180, 180, 100);
+
   // ..........................................................................
 }
 
@@ -93,11 +207,11 @@ void usercontrol(void) {
 
     //Simple intake on top right bumper
     if (Controller1.ButtonR1.pressing()) {
-      IntakeL.spin(forward, 100, vex::velocityUnits::pct);
-      IntakeR.spin(forward, 100, vex::velocityUnits::pct);
+      IntakeL.spin(forward, 127, vex::voltageUnits::volt);
+      IntakeR.spin(forward, 127, vex::voltageUnits::volt);
     } else if(Controller1.ButtonR2.pressing()) { //Simple outtake on bottom right bumper
-      IntakeL.spin(reverse, 100, vex::velocityUnits::pct);
-      IntakeR.spin(reverse, 100, vex::velocityUnits::pct);  
+      IntakeL.spin(reverse, 127, vex::voltageUnits::volt);
+      IntakeR.spin(reverse, 127, vex::voltageUnits::volt);
     } else {
       IntakeL.stop(vex::brakeType::hold);
       IntakeR.stop(vex::brakeType::hold);
@@ -106,11 +220,11 @@ void usercontrol(void) {
     //TODO indexer speed set to low value for testing so we don't break axles
     //Simple indexer up on top left bumper
     if (Controller1.ButtonL1.pressing()) {
-      IndexerL.spin(forward, 127, vex::velocityUnits::pct);
-      IndexerR.spin(forward, 127, vex::velocityUnits::pct);
+      IndexerL.spin(forward, 127, vex::voltageUnits::volt);
+      IndexerR.spin(forward, 127, vex::voltageUnits::volt);
     } else if(Controller1.ButtonL2.pressing()) { //Simple indexer down on bottom left bumper
-      IndexerL.spin(reverse, 100, vex::velocityUnits::pct);
-      IndexerR.spin(reverse, 100, vex::velocityUnits::pct);  
+      IndexerL.spin(reverse, 127, vex::voltageUnits::volt);
+      IndexerR.spin(reverse, 127, vex::voltageUnits::volt);
     } else {
       IndexerL.stop(vex::brakeType::hold);
       IndexerR.stop(vex::brakeType::hold);
@@ -118,7 +232,7 @@ void usercontrol(void) {
 
     // ........................................................................
 
-    wait(20, msec); // Sleep the task for a short amount of time to
+    wait(10, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
 }
