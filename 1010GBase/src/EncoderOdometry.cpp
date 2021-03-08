32907397192354fdeb6bcd2631def71d2f08466b
PhillipRojkov <rojkovphillip@gmail.com>
2021-03-07 20:36:29 -0800

    removed if statement in odometry

--- 1010GBase/src/EncoderOdometry.cpp
+++ 1010GBase/src/EncoderOdometry.cpp
@@ -1,32 +1,29 @@
 #include "EncoderOdometry.h"
 
 void EncoderOdometry::computeLocation() {
-  deltaL = (encoderL.position(deg) - prevEncoderL) / 180 * PI * wheelRadius;
-  deltaR = (encoderR.position(deg) - prevEncoderR) / 180 * PI * wheelRadius;
-  deltaS = (encoderS.position(deg) - prevEncoderS) / 180 * PI * wheelRadius;
+  double encoderLValue = encoderL.position(deg);
+  double encoderRValue = encoderR.position(deg);
+  double encoderSValue = encoderS.position(deg);
 
-  prevEncoderL = encoderL.position(deg);
-  prevEncoderR = encoderR.position(deg);
-  prevEncoderS = encoderS.position(deg);
+  deltaL = (encoderLValue - prevEncoderL) / 180 * PI * wheelRadius;
+  deltaR = (encoderRValue - prevEncoderR) / 180 * PI * wheelRadius;
+  deltaS = (encoderSValue - prevEncoderS) / 180 * PI * wheelRadius;
+
+  prevEncoderL = encoderLValue;
+  prevEncoderR = encoderRValue;
+  prevEncoderS = encoderSValue;
 
   deltaTheta = (deltaL - deltaR) / (offsetL + offsetR);
   theta += deltaTheta;
 
-  double arcRadius = 0;
-  double strafeRadius = 0;
-  if (fabs(deltaTheta) < 0.001) {
-    arcRadius = offsetR;
-    strafeRadius = offsetS;
+  double arcRadius = 0; //radius of the motion of the robot modeled as an arc
+  double strafeRadius = 0; //radius of the strafe motion of the robot modeled as an arc
 
-    deltaX = deltaS;
-    deltaY = deltaR;
-  } else {
-    arcRadius = deltaR / deltaTheta + offsetR;
-    strafeRadius = deltaS / deltaTheta + offsetS;
+  arcRadius = deltaR / deltaTheta + offsetR;
+  strafeRadius = deltaS / deltaTheta + offsetS; //Radius of the strafe arc
 
-    deltaX = 2 * sin(deltaTheta / 2) * strafeRadius;
-    deltaY = 2 * sin(deltaTheta / 2) * arcRadius;
-  }
+  deltaX = 2 * sin(deltaTheta / 2) * strafeRadius;
+  deltaY = 2 * sin(deltaTheta / 2) * arcRadius;
 
   double avgTheta = (theta + prevTheta) / 2;
 
